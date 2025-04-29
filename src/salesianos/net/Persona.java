3efb50be0ef03cd1b0d9bef0aef6a28f61ceeee2
TodoPoderosoCesar <usr3965@salesianos-lacuesta.net>
2025-04-29 09:55:02 +0100

    feat(Person.java): added all properties

--- src/salesianos/net/Persona.java
+++ src/salesianos/net/Persona.java
@@ -1,5 +1,28 @@
 package salesianos.net;
 
 public class Persona {
+    private int age;
+    private String name;
+
+    public Persona(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
 }
