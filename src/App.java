594a3d33ab5daf36aac5437f747346ef25ebb687
TodoPoderosoCesar <usr3965@salesianos-lacuesta.net>
2025-04-29 09:15:43 +0100

    First commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+// git init
+// git branch -M main
+// Creamos el .gitignore
+
+
+// git add .
+// git commit -m "first commit"
+// git remote add origin https://github.com/yourusername/yourproject.git
+// git push -u origin master
